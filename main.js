5e068fc3c81e6f8f36215a239f5c69db1d1d1c86
Godefery <hiyangguo@qq.com>
2016-08-11 19:56:43 +0800

    新增测试文件

--- main.js
+++ main.js
@@ -2,8 +2,4 @@
  * Created by Godfery on 2016/8/8 0008.
  */
 var build = require('./src/build');
-build({
-    color: '#1b9451',
-    output: 'abc/bcd'
-});
 module.exports = build;
---
+++ test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Godfery on 2016/8/11 0011.
+ */
+var build = require('./main');
+build({
+    color: '#1b9451',
+    output: 'rsuite/css'
+});
